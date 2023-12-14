053afa4336e82054d42d22e81108cafbcf294735
Hamed-barzingar <153176970+Hamed-barzingar@users.noreply.github.com>
2023-12-14 12:34:31 +0330

    Add files via upload

---
+++ Tamrin 1.py
@@ -0,0 +1,12 @@
+a = int(input('Enter your namber : '))
+b = 0
+b = a * 2
+x = 0
+
+while a < b :
+	v =  int(input('enter your namber : '))
+	x = x + v
+	a = a + 1
+a = a/2
+print ( 'jame' , x)
+print ( "miangin = " ,  x/a )
